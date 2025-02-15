c0662adcb3f893ea1ed49e66433eb19b313edf1b
AndreyBurkov <burkov_andrey_alex@mail.ru>
2025-02-15 13:11:26 +0300

    commit

--- src/main/java/com/example/SpringBootExampleApplication.java
+++ src/main/java/com/example/SpringBootExampleApplication.java
@@ -1,13 +1,12 @@
 package com.example;
 
-import com.example.eployees.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
+/*
+docker run --name spring-boot-example -p 5432:5432 -e POSTGRES_PASSWORD=root -d postgres
+
+ */
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
